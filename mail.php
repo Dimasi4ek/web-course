89fe8eeee57deeb54d568b2fa071ba384fbf3b91
drafti <39197572+Dimasi4ek@users.noreply.github.com>
2021-03-12 22:39:20 +0200

    1

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+
+/* https://api.telegram.org/bot1015844949:AAGg4dq9x6ewi0T4ArLSTh3O63Zxs8YEV7A/getUpdates,
+где, 1015844949:AAGg4dq9x6ewi0T4ArLSTh3O63Zxs8YEV7A - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$token = "1015844949:AAGg4dq9x6ewi0T4ArLSTh3O63Zxs8YEV7A";
+$chat_id = "529661165";
+$arr = array(
+  'Имя клиента: ' => $name,
+  'Телефон: ' => "$phone",
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+?>
